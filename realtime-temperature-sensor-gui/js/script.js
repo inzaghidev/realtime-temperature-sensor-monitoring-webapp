6faae19bd195576fcdb64cbca895b8399491f020
Inzaghi's Dev <139905564+inzaghidev@users.noreply.github.com>
2023-10-23 22:13:49 +0700

    Add files via upload

--- realtime-temperature-sensor-gui/js/script.js
+++ realtime-temperature-sensor-gui/js/script.js
@@ -1,34 +1,127 @@
+// Array to store sensor data
+let sensorsData = [];
+
+// Add Sensor button click handler
+$("#addSensorBtn").on("click", function () {
+  // Show a dialog to add a new sensor
+  const sensorName = prompt("Enter Sensor Name :");
+  if (sensorName !== null && sensorName.trim() !== "") {
+    addSensor(sensorName);
+  }
+});
+
+// Delete Sensor button click handler
+$(document).on("click", ".deleteSensorBtn", function () {
+  const sensorId = $(this).data("sensor-id");
+  const confirmDelete = confirm("Are you want to Delete this Sensor?");
+  if (confirmDelete) {
+    // Call a function to delete the sensor with the given ID
+    deleteSensor(sensorId);
+  }
+});
+
+// Function to update the circular progress bar and text for each sensor
 // Simulate real-time temperature updates
 function updateTemperature() {
   setInterval(function () {
-    // Simulate temperature values between 20 and 40 degrees Celsius
-    let sensor1Value = Math.random() * 20 + 20;
-    let sensor2Value = Math.random() * 20 + 20;
-    let sensor3Value = Math.random() * 20 + 20;
-    let sensor4Value = Math.random() * 20 + 20;
-    let sensor5Value = Math.random() * 20 + 20;
-    let sensor6Value = Math.random() * 20 + 20;
+    sensorsData.forEach((sensor) => {
+      const temperature = Math.random() * 20 + 20; // Simulate temperature values between 20 and 40 degrees Celsius
+      sensor.temperature = temperature;
 
-    // Update the circular progress bar and text for each sensor
-    updateCircularProgressBar("sensor1Progress", sensor1Value, "1");
-    updateCircularProgressBar("sensor2Progress", sensor2Value, "2");
-    updateCircularProgressBar("sensor3Progress", sensor3Value, "3");
-    updateCircularProgressBar("sensor4Progress", sensor4Value, "4");
-    updateCircularProgressBar("sensor5Progress", sensor5Value, "5");
-    updateCircularProgressBar("sensor6Progress", sensor6Value, "6");
+      // Update the circular progress bar and text for each sensor
+      updateCircularProgressBar(sensor.id, temperature, sensor.number);
+    });
   }, 2000); // Update every 2 seconds
 }
 
-// Update the circular progress bar based on temperature value
+// Function to update the circular progress bar based on temperature value
 function updateCircularProgressBar(elementId, temperature, textElementId) {
-  let progress = ((temperature - 20) / 10) * 360; // Map temperature to degrees
+  const progress = ((temperature - 20) / 20) * 360; // Map temperature to degrees
   $("#" + elementId).text(temperature.toFixed(2) + " °C");
-  $("#" + textElementId).text(
+  $("#" + textElementId + "Text").text(
     "Sensor " + textElementId + ": " + temperature.toFixed(2) + " °C"
+  );
+
+  // Animate the circular progress
+  $("#" + elementId).animate(
+    { deg: progress },
+    {
+      duration: 1000, // Animation duration in milliseconds
+      step: function (now) {
+        $("#" + elementId).css("transform", "rotate(" + now + "deg)");
+      },
+    }
   );
 }
 
 // Start updating temperature values
 $(document).ready(function () {
+  // Initialize sensors data when the document is ready
+  for (let i = 1; i <= 6; i++) {
+    const sensorId = "sensor" + i + "Progress";
+    const sensorTextId = "sensor" + i + "Text";
+    const sensorName = "Sensor " + i;
+
+    // Add sensor data to the array
+    sensorsData.push({
+      id: sensorId,
+      number: i,
+      temperature: 0,
+    });
+
+    // Add sensor card to the DOM
+    addSensorCard(sensorId, sensorTextId, sensorName);
+  }
+
+  // Start updating temperature values
   updateTemperature();
 });
+
+// Function to add a new sensor
+function addSensor(sensorName) {
+  const sensorNumber = sensorsData.length + 1;
+  const sensorId = "sensor" + sensorNumber + "Progress";
+  const sensorTextId = "sensor" + sensorNumber + "Text";
+
+  // Add sensor data to the array
+  sensorsData.push({
+    id: sensorId,
+    number: sensorNumber,
+    temperature: 0,
+  });
+
+  // Add sensor card to the DOM
+  addSensorCard(sensorId, sensorTextId, sensorName);
+}
+
+// Function to delete a sensor
+function deleteSensor(sensorId) {
+  // Remove sensor data from the array
+  sensorsData = sensorsData.filter((sensor) => sensor.id !== sensorId);
+
+  // Remove sensor card from the DOM
+  $("#" + sensorId)
+    .closest(".col-lg-4")
+    .remove();
+}
+
+// Function to add a sensor card to the DOM
+function addSensorCard(sensorId, sensorTextId, sensorName) {
+  const newSensorCard = `
+    <div class="col-lg-4 col-md-6" style="padding: 10px">
+      <div class="card sensor-card text-center">
+        <div class="card-body">
+          <div class="circular-progress">
+            <div class="progress-value" id="${sensorId}">
+              Loading...
+            </div>
+          </div>
+          <p class="text" id="${sensorTextId}">${sensorName}</p>
+          <button class="btn btn-danger deleteSensorBtn" data-sensor-id="${sensorId}">Delete</button>
+        </div>
+      </div>
+    </div>
+  `;
+
+  $("#sensorContainer").append(newSensorCard);
+}
